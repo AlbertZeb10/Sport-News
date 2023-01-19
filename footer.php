7c0c505a1c83653987e34748ed354af42ecea121
AlbertZeb10 <108046490+AlbertZeb10@users.noreply.github.com>
2023-01-19 14:25:00 +0700

    first commit

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer >
+		<div class="text-center text-white p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+			<p>Albert Saputra Zebua - 412020012 UKRIDA</p>
+			<br>
+			<p>Project Pemrograman Web I</p>
+			<br>
+            <h5>Copyright ©News About SPORTNEWS 2021 by Albert</h5>
+		</div>	
+		</footer>
